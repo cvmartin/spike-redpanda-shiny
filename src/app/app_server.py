533912e8425736b451a5ff5c5d1bae14287480ad
cvmartin <cvarelamail@gmail.com>
2023-07-31 20:19:33 +0200

    application works with incoming kafka

--- src/app/app_server.py
+++ src/app/app_server.py
@@ -1,5 +1,41 @@
 """Server side of application."""
-from shiny import Inputs, Outputs, Session, render, ui
+import asyncio
+import json
+from typing import Any, AsyncGenerator
+
+from aiokafka import AIOKafkaConsumer, ConsumerRecord
+from shiny import Inputs, Outputs, Session, reactive, render
+
+KafkaMessage = dict[str, Any]
+
+
+async def consume_kafka_topic(
+    topic_name: str,
+    bootstrap_servers: str,
+) -> AsyncGenerator[str, None]:
+    """Consume asynchronously from a kafka topic."""
+    consumer = AIOKafkaConsumer(
+        topic_name,
+        loop=asyncio.get_event_loop(),
+        bootstrap_servers=bootstrap_servers,
+    )
+
+    await consumer.start()
+
+    try:
+        message: ConsumerRecord[bytes, bytes]
+        async for message in consumer:
+            yield message.value.decode("utf-8")
+    finally:
+        await consumer.stop()
+
+
+async def watch_changes(rval: reactive.Value[KafkaMessage]) -> None:
+    async for message in consume_kafka_topic(
+        topic_name="meter_measurements", bootstrap_servers="localhost:9092"
+    ):
+        rval.set(json.loads(message))
+        await reactive.flush()
 
 
 def app_server(
@@ -7,7 +43,17 @@ def app_server(
     output: Outputs,
     session: Session,  # noqa: ARG001
 ) -> None:
-    @output(id="txt")
+    # App setup
+    incoming_val = reactive.Value({})
+
+    # following https://beta.ruff.rs/docs/rules/asyncio-dangling-task/
+    background_tasks = set()
+    task = asyncio.create_task(watch_changes(incoming_val))
+    background_tasks.add(task)
+    task.add_done_callback(background_tasks.discard)
+
+    # App output
+    @output(id="async_text")
     @render.text
     def _():
-        return f"n*2 is {input.n() * 2}"
+        return str(incoming_val.get())
--- src/app/app_ui.py
+++ src/app/app_ui.py
@@ -3,6 +3,6 @@
 from shiny import ui
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    "This should update automatically",
+    ui.output_text_verbatim("async_text"),
 )
