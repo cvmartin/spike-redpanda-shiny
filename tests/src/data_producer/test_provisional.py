4caf8fad0f009c9a0af9b256b2bd229a12207aff
cvmartin <cvarelamail@gmail.com>
2023-08-08 10:53:47 +0200

    improved test_provisional

--- tests/src/data_producer/test_provisional.py
+++ tests/src/data_producer/test_provisional.py
@@ -4,7 +4,11 @@ from typing import Any
 from uuid import uuid4
 
 import pytest
-from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry import (
+    RegisteredSchema,
+    Schema,
+    SchemaRegistryClient,
+)
 from confluent_kafka.schema_registry.avro import AvroDeserializer, AvroSerializer
 from confluent_kafka.serialization import (
     MessageField,
@@ -13,28 +17,98 @@ from confluent_kafka.serialization import (
 from kafka import KafkaConsumer, KafkaProducer, TopicPartition
 from kafka.consumer.fetcher import ConsumerRecord
 
-from data_producer.data_producer import retrieve_schema_from_registry
+
+class AvroParser:
+    def __init__(
+        self,
+        schema_registry_client: SchemaRegistryClient,
+        schema_subject: str,
+        schema_version: int,
+        topic_name: str,
+    ) -> None:
+        self.schema_registry_client = schema_registry_client
+        self.schema_subject = schema_subject
+        self.schema_version = schema_version
+        self.topic_name = topic_name
+
+        self.key_schema: str = '{"type": "string"}'
+
+        self.registered_value_schema: RegisteredSchema = (
+            self.schema_registry_client.get_version(
+                self.schema_subject,
+                self.schema_version,
+            )
+        )
+        self.value_schema: Schema = self.registered_value_schema.schema.schema_str
+
+        self.avro_key_serializer = AvroSerializer(
+            conf={"auto.register.schemas": False},
+            schema_registry_client=self.schema_registry_client,
+            schema_str=self.key_schema,
+        )
+
+        self.avro_key_deserializer = AvroDeserializer(
+            schema_registry_client=self.schema_registry_client,
+            schema_str=self.key_schema,
+        )
+
+        self.avro_value_serializer = AvroSerializer(
+            conf={"auto.register.schemas": False},
+            schema_registry_client=self.schema_registry_client,
+            schema_str=self.value_schema,
+        )
+
+        self.avro_value_deserializer = AvroDeserializer(
+            schema_registry_client=self.schema_registry_client,
+            schema_str=self.value_schema,
+        )
+
+        self.key_context = SerializationContext(
+            self.topic_name,
+            MessageField.KEY,
+        )
+
+        self.value_context = SerializationContext(
+            self.topic_name,
+            MessageField.VALUE,
+        )
+
+    def serialize_key(self, obj: object) -> bytes:
+        return self.avro_key_serializer(  # type: ignore
+            obj=obj,
+            ctx=self.key_context,
+        )
+
+    def serialize_value(self, obj: object) -> bytes:
+        return self.avro_value_serializer(  # type: ignore
+            obj=obj,
+            ctx=self.value_context,
+        )
+
+    def deserialize_key(self, data: bytes) -> object:
+        return self.avro_key_deserializer(  # type: ignore
+            data=data,
+            ctx=self.key_context,
+        )
+
+    def deserialize_value(self, data: bytes) -> object:
+        return self.avro_value_deserializer(  # type: ignore
+            data=data,
+            ctx=self.value_context,
+        )
 
 
-# @pytest.mark.skip(reason="depends on locally deployed broker")
+@pytest.mark.skip(reason="depends on locally deployed broker")
 def test_provisional():
     address_registry = "http://localhost:8081"
     bootstrap_server = "localhost:9092"
+    message_topic = "testing_schemas"
 
-    schema_registry_client = SchemaRegistryClient(conf={"url": address_registry})
-
-    avro_key_serializer = AvroSerializer(
-        schema_registry_client=schema_registry_client,
-        schema_str='{"type": "string"}',
-    )
-
-    avro_value_serializer = AvroSerializer(
-        schema_registry_client=schema_registry_client,
-        schema_str=retrieve_schema_from_registry(
-            schema_registry_url=address_registry,
-            subject="meter_measurements-value",
-            version=1,
-        ),
+    test_parser = AvroParser(
+        schema_registry_client=SchemaRegistryClient(conf={"url": address_registry}),
+        schema_subject="meter_measurements-value",
+        schema_version=1,
+        topic_name=message_topic,
     )
 
     message_key: str = str(uuid4())
@@ -47,24 +121,10 @@ def test_provisional():
         ),
     }
 
-    message_topic = "testing_schemas"
-
     producer = KafkaProducer(
         bootstrap_servers=[bootstrap_server],
-        key_serializer=lambda x: avro_key_serializer(
-            x,
-            SerializationContext(
-                message_topic,
-                MessageField.KEY,
-            ),
-        ),
-        value_serializer=lambda x: avro_value_serializer(
-            x,
-            SerializationContext(
-                message_topic,
-                MessageField.VALUE,
-            ),
-        ),
+        key_serializer=test_parser.serialize_key,
+        value_serializer=test_parser.serialize_value,
     )
 
     producer.send(
@@ -72,41 +132,12 @@ def test_provisional():
         key=message_key,
         value=message_value,
     )
-
     producer.flush()
-
-    ####
-
-    avro_key_deserializer = AvroDeserializer(
-        schema_registry_client=schema_registry_client,
-        schema_str='{"type": "string"}',
-    )
-
-    avro_value_deserializer = AvroDeserializer(
-        schema_registry_client=schema_registry_client,
-        schema_str=retrieve_schema_from_registry(
-            schema_registry_url=address_registry,
-            subject="meter_measurements-value",
-            version=1,
-        ),
-    )
 
     consumer = KafkaConsumer(
         bootstrap_servers=[bootstrap_server],
-        key_deserializer=lambda x: avro_key_deserializer(
-            x,
-            SerializationContext(
-                message_topic,
-                MessageField.KEY,
-            ),
-        ),
-        value_deserializer=lambda x: avro_value_deserializer(
-            x,
-            SerializationContext(
-                message_topic,
-                MessageField.VALUE,
-            ),
-        ),
+        key_deserializer=test_parser.deserialize_key,
+        value_deserializer=test_parser.deserialize_value,
     )
 
     tp = TopicPartition(message_topic, 0)
